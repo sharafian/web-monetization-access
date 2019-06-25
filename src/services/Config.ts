ca23243be7b05bfcd5d1697a97bffd98be120ef1
sharafian <ben@sharafian.me>
2019-06-24 17:45:12 -0700

    feat: produce proof of payment

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -4,4 +4,16 @@ export class Config {
   public port: string = process.env.PORT || '8080'
   public connectionTagKey: string = process.env.CONNECTION_TAG_KEY ||
     crypto.randomBytes(32).toString('base64')
+  public proofKey: string = process.env.PROOF_KEY ||
+		crypto.generateKeyPairSync('ec', {
+			namedCurve: 'secp256k1',
+			publicKeyEncoding: {
+				type: 'spki',
+				format: 'pem'
+			},
+			privateKeyEncoding: {
+				type: 'pkcs8',
+				format: 'pem'
+			}
+		}).privateKey
 }
--- src/services/Proof.ts
+++ src/services/Proof.ts
@@ -1,18 +1,53 @@
 import { MemoryStore } from './MemoryStore'
+import { Config } from './Config'
 import { Injector } from 'reduct'
 import { Context } from 'koa'
 import Router from 'koa-router'
+import * as jwt from 'jsonwebtoken'
+import * as crypto from 'crypto'
 
 export class Proof {
   private store: MemoryStore
+  private config: Config
+  private privateKey: string
+  private publicKey: string
 
   constructor (deps: Injector) {
     this.store = deps(MemoryStore)
+    this.config = deps(Config)
+    this.privateKey = this.config.proofKey
+    this.publicKey = crypto.createPublicKey(this.privateKey)
+      .export({
+        type: 'spki',
+        format: 'pem'
+      })
+      .toString('utf8')
   }
 
   async start (router: Router) {
-    router.get('/proof/:id', async (ctx: Context) => {
-      ctx.body = this.store.get(ctx.params.id)
+    router.get('/pay/public_key', async (ctx: Context) => {
+      ctx.body = {
+        public_key: this.publicKey
+      }
+    })
+
+    router.get('/pay/proof/:id', async (ctx: Context) => {
+      const data = this.store.get(ctx.params.id)
+      const token: string = await new Promise((resolve: Function, reject: Function) => {
+        jwt.sign(data, this.privateKey, {
+          algorithm: 'ES256'
+        }, (err: Error, encoded: string) => {
+          if (err) {
+            reject(err) 
+          } else {
+            resolve(encoded)
+          }
+        })
+      })
+
+      ctx.body = {
+        token
+      }
     })
   }
 }
